a692fe45239169341bce716cb05f7d4ff7548f58
Raeef Chowdhury <raeef.chowdhury1@education.nsw.gov.au>
2026-02-03 22:07:24 +1100

    feat(ui): handle edge cases for priority section

--- components/cards/PrioritiesCard.tsx
+++ components/cards/PrioritiesCard.tsx
@@ -2,7 +2,6 @@ import { PriorityColors } from "@/Data/MockPriorities";
 import { Todo } from "@/Types/TodoItemTypes";
 import { useDaysLeft } from "@/hooks/Todo/useTodoDateConversion";
 import { TriangleAlert } from "lucide-react";
-import { useState } from "react";
 
 export function PrioritiesCard({
   colors,
@@ -11,7 +10,6 @@ export function PrioritiesCard({
   colors: PriorityColors;
   todo: Todo;
 }) {
-  const [isExpanded, setIsExpanded] = useState(false);
   const { getDaysLeftText, calculateDaysLeft, isOverdue, getDaysLeftStyles } =
     useDaysLeft();
   const daysLeft = todo.dueDate ? calculateDaysLeft(todo.dueDate) : null;
@@ -19,10 +17,8 @@ export function PrioritiesCard({
   return (
     <div
       key={todo.id}
-      className={`border-l-2 flex flex-col gap-4 ${colors.border} bg-secondary/10 rounded-lg p-6 hover:bg-secondary/20 transition-all duration-200 cursor-pointer relative group ${
-        isExpanded ? "z-10" : ""
-      }`}
-      onClick={() => setIsExpanded(!isExpanded)}
+      className={`border-l-2 flex flex-col gap-4 ${colors.border} bg-secondary/10 rounded-lg p-6 hover:bg-secondary/20 transition-all duration-200 cursor-pointer relative group 
+        `}
     >
       {todo.priority && (
         <span
@@ -31,56 +27,49 @@ export function PrioritiesCard({
           {todo.priority}
         </span>
       )}
-
       <div className="pr-20">
         <h3
-          className={`text-xl mb-8 font-semibold text-white leading-tight capitalize ${
-            isExpanded ? "" : "line-clamp-2"
-          } transition-all duration-200`}
+          className={`text-xl mb-8 font-semibold text-white leading-tight capitalize line-clamp-2 transition-all duration-200`}
           title={todo.title}
         >
           {todo.title}
         </h3>
       </div>
-
-      <div className="flex items-center flex-wrap gap-3 text-sm">
+      <div className="flex items-center gap-3 text-sm">
         {todo.tags && todo.tags.length > 1 && (
           <>
-            {todo.tags
-              .slice(0, isExpanded ? undefined : 8)
-              .map((tag, index) => (
-                <span
-                  key={index}
-                  className={`text-xs ${tag.length > 50 ? "truncate" : ""} px-2.5 py-1 capitalize bg-secondary/20 text-accent/70 rounded font-medium`}
-                >
-                  {tag}
-                </span>
-              ))}
-            {!isExpanded && todo.tags.length > 2 && (
+            {todo.tags.slice(0, 2).map((tag, index) => (
+              <span
+                key={index}
+                className={`text-xs ${tag.length > 50 ? "truncate" : ""} px-2.5 py-1 capitalize bg-secondary/20 text-accent/70 rounded font-medium`}
+              >
+                {tag}
+              </span>
+            ))}
+            {todo.tags.length > 2 && (
               <span className="text-xs text-gray-500 font-medium">
                 +{todo.tags.length - 2}
               </span>
             )}
           </>
         )}
-
-        {daysLeft !== null && (
-          <div className="flex items-center gap-1.5 ml-auto">
-            {isOverdue(daysLeft) && (
-              <TriangleAlert className="w-4 h-4 text-high-priority" />
-            )}
-            <span
-              className={`text-xs font-medium px-3 py-1 rounded-md ${getDaysLeftStyles(daysLeft)} ${
-                isOverdue(daysLeft)
-                  ? "bg-high-priority/10 text-high-priority"
-                  : "bg-secondary/20 text-gray-400"
-              }`}
-            >
-              {getDaysLeftText(daysLeft)}
-            </span>
-          </div>
-        )}
-      </div>
+      </div>{" "}
+      {daysLeft !== null && (
+        <div className="flex items-center justify-self-start gap-1.5 mr-auto">
+          {isOverdue(daysLeft) && (
+            <TriangleAlert className="w-4 h-4 text-high-priority" />
+          )}
+          <span
+            className={`text-xs font-medium px-3 py-1 rounded-md ${getDaysLeftStyles(daysLeft)} ${
+              isOverdue(daysLeft)
+                ? "bg-high-priority/10 text-high-priority"
+                : "bg-secondary/20 text-gray-400"
+            }`}
+          >
+            {getDaysLeftText(daysLeft)}
+          </span>
+        </div>
+      )}
     </div>
   );
 }
--- components/section/Priorities.tsx
+++ components/section/Priorities.tsx
@@ -2,8 +2,9 @@
 import { PrioritiesCard } from "@/components/cards/PrioritiesCard";
 import { IncompletePriorities } from "@/components/Fallback/IncompletePriorities";
 import { EmptyPriorities } from "../Fallback/EmptyPriorities";
-
+import { ArrowRight } from "lucide-react";
 import { useTodoSortDasboard } from "@/hooks/dashboard/useTodoSortDashboard";
+import Link from "next/link";
 
 function Priorities() {
   const { sortedTodos } = useTodoSortDasboard();
@@ -37,9 +38,16 @@ function Priorities() {
 
   return (
     <section className="mt-32 w-full max-w-[85vw] mx-auto">
-      <h2 className="text-[1.8rem] font-semibold tracking-tight text-white text-left">
-        Your Top Priorities
-      </h2>
+      <div className="flex justify-between mb-12 items-start">
+        <h2 className="text-[1.8rem]    font-semibold tracking-tight text-white text-left">
+          Your Top Priorities
+        </h2>
+        <Link href="/tasks">
+          <button className="text-accent/70 mt-5  text-xs hover:scale-105 transition-all hover:cursor-pointer hover:text-accent underline underline-offset-4   gap-1  flex items-center">
+            View all Tasks <ArrowRight className="w-4 h-4" />
+          </button>
+        </Link>
+      </div>
       <div className="grid grid-cols-3 gap-12 mt-[1.6rem] items-start">
         {sortedTodos.map((todo) => {
           const colors = priorityColors[todo.priority || ""];
